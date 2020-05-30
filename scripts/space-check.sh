32fd50b573be87bceb9a67c285eca755ea41caa1
githubuserpk <pradip.tk@gmail.com>
2020-05-30 16:59:47 +0100

    Create space-check.sh

---
+++ scripts/space-check.sh
@@ -0,0 +1,11 @@
+echo "==========================================="
+echo "running df -h to get break-up of sub folders"
+echo "==========================================="
+df -h
+
+echo "==========================================="
+echo "running du -sk * to get break-up of sub folders"
+echo "==========================================="
+
+#run from home directory to know the split
+du -hs *
