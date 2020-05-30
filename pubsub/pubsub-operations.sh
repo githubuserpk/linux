e0534a8b33433b0795baeb5b60092810fff9c2e5
githubuserpk <pradip.tk@gmail.com>
2020-05-30 17:20:43 +0100

    Create pubsub-operations.sh

---
+++ pubsub/pubsub-operations.sh
@@ -0,0 +1,43 @@
+#create pubsub topic
+gcloud pubsub topics create myTopic
+
+#create another topic
+gcloud pubsub topics create Test1
+
+#list the topics
+gcloud pubsub topics list
+
+#delete topic
+gcloud pubsub topics delete Test1
+
+#============================================
+
+#create a subscription
+gcloud  pubsub subscriptions create --topic myTopic mySubscription
+
+#you can also create more subscriptions
+#gcloud  pubsub subscriptions create --topic myTopic mysub2
+#gcloud  pubsub subscriptions create --topic myTopic mysub3
+
+#list subscriptions
+gcloud pubsub topics list-subscriptions myTopic
+
+
+#publish message to the mytopic 
+#mesg-1
+gcloud pubsub topics publish myTopic --message "Hello pk"
+
+#mesg-2
+gcloud pubsub topics publish myTopic --message "Publisher likes to eat pizza"
+
+#below command gets one message ie consumes one message out at a time
+gcloud pubsub subscriptions pull mySubscription --auto-ack --limit=3
+
+
+#below command outputs 3 messages at a time, ie consumes 3 messages at a time
+gcloud pubsub subscriptions pull mySubscription --auto-ack --limit=3
+
+
+
+
+
