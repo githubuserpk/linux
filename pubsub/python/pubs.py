76c4e746125da1acf3997295ff1d96b01f6ab059
githubuserpk <pradip.tk@gmail.com>
2020-07-30 19:25:42 +0100

    Create pubs.py

---
+++ pubsub/python/pubs.py
@@ -0,0 +1,23 @@
+# Reference url: https://cloud.google.com/pubsub/docs/quickstart-client-libraries
+# In this example we publish 10 messages to an existing topic called myTopic
+
+from google.cloud import pubsub_v1
+
+# TODO(developer)
+project_id = "intense-cortex-278011"
+topic_id = "myTopic"
+
+publisher = pubsub_v1.PublisherClient()
+# The `topic_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/topics/{topic_id}`
+topic_path = publisher.topic_path(project_id, topic_id)
+
+for n in range(1, 10):
+    data = u"Message number {}".format(n)
+    # Data must be a bytestring
+    data = data.encode("utf-8")
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, data=data)
+    print(future.result())
+
+print("Published messages.")
