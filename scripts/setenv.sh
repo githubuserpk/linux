f7aa6f64747dd0cfe7eb651c5eed172569e12f4a
githubuserpk <pradip.tk@gmail.com>
2020-07-30 21:06:37 +0100

    Create setenv.sh

---
+++ scripts/setenv.sh
@@ -0,0 +1,5 @@
+export GOOGLE_PROJECT=$(gcloud config get-value project)
+export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
+export PROJECT_ID=$(gcloud config get-value project)
+alias python=python3
+alias pip=pip3
