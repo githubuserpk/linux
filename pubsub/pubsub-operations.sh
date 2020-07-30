86ef9c328a5a3821b2a8231561e69b98959f5c23
githubuserpk <pradip.tk@gmail.com>
2020-07-30 15:06:12 +0100

    Update pubsub-operations.sh

--- pubsub/pubsub-operations.sh
+++ pubsub/pubsub-operations.sh
@@ -25,14 +25,19 @@ gcloud pubsub topics list-subscriptions myTopic
 
 #publish message to the mytopic 
 #mesg-1
-gcloud pubsub topics publish myTopic --message "Hello pk"
+gcloud pubsub topics publish myTopic --message "1. Hello pk"
 
 #mesg-2
-gcloud pubsub topics publish myTopic --message "Publisher wants to order a pizza"
+gcloud pubsub topics publish myTopic --message "2. customer wants to order a pizza"
 
 #below command gets one message ie consumes one message out at a time
 gcloud pubsub subscriptions pull mySubscription --auto-ack --limit=1
 
+#mesg-3
+gcloud pubsub topics publish myTopic --message "2. customer wants to order a pizza"
+gcloud pubsub topics publish myTopic --message "3. order placed"
+gcloud pubsub topics publish myTopic --message "4. order delivered"
+gcloud pubsub topics publish myTopic --message "5. customer gave thumbs up"
 
 #below command outputs 3 messages at a time, ie consumes 3 messages at a time
 gcloud pubsub subscriptions pull mySubscription --auto-ack --limit=3
