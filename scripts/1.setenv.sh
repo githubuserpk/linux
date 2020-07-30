db0f6cb95f4e99d66ea01eeefd5ba45c0d60a4b8
githubuserpk <pradip.tk@gmail.com>
2020-07-30 21:17:51 +0100

    Update 1.setenv.sh

--- scripts/1.setenv.sh
+++ scripts/1.setenv.sh
@@ -3,3 +3,5 @@ export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
 export PROJECT_ID=$(gcloud config get-value project)
 alias python=python3
 alias pip=pip3
+
+export GOOGLE_APPLICATION_CREDENTIALS=~/owner-sa.json
