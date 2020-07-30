d405cd3cfcb75321e6039b0ae15af46fb9fc27cc
githubuserpk <pradip.tk@gmail.com>
2020-07-30 21:42:11 +0100

    Update publisher.py

--- pubsub/python/publisher.py
+++ pubsub/python/publisher.py
@@ -1,10 +1,13 @@
 # Reference url: https://cloud.google.com/pubsub/docs/quickstart-client-libraries
 # In this example we publish 10 messages to an existing topic called myTopic
-
+import os
 from google.cloud import pubsub_v1
 
+project_id =os.environ['PROJECT_ID']
+print("project id: ", project_id)
+
 # TODO(developer)
-project_id = "intense-cortex-278011"
+# project_id = "intense-cortex-278011"
 topic_id = "myTopic"
 
 publisher = pubsub_v1.PublisherClient()
